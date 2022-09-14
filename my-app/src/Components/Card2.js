57edbd7e089890e16419734591f1b2afd453c474
tofayel <tajuddinahmed450@gmail.com>
2022-09-14 10:53:30 +0600

    practice state, implement a counter with state

--- my-app/src/Components/Card2.js
+++ my-app/src/Components/Card2.js
@@ -5,11 +5,37 @@ import { Button } from 'react-bootstrap';
 
 class Card2 extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            count : 0
+        }
+
+    }
+
+    increment = () => {
+        console.log("clicked");
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+
+    decrement = () =>{
+        if(this.state.count != 0){
+            this.setState({
+                count : this.state.count - 1
+            })
+        }
+    }
+
     render(){
         return <div>
             <h2>hello class Component, {this.props.name}</h2>
             <FaReact className="icon"/>
-            <Button variant="warning"> Learn More</Button>
+            <h2>Count : {this.state.count}</h2>
+            <Button className="p-3 m-4" onClick={this.increment} variant="warning"> + </Button>
+            <Button className="p-3" onClick={this.decrement}> - </Button>
         </div>
     }
 
